f542f6c1b94fde90fe12e447035c3dbf2ed9d132
jcglezi <42814036+jcglezi@users.noreply.github.com>
2019-03-11 01:40:32 -0600

    ultima version

--- ExamenPractico/Controllers/HomeController.cs
+++ ExamenPractico/Controllers/HomeController.cs
@@ -1,46 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using ExamenPractico.Models;
+using ExamenPractico.Models.Entidades;
+using Newtonsoft.Json;
 
 namespace ExamenPractico.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            HttpClient httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync("https://samples.openweathermap.org/data/2.5/weather?lat=19&lon=-99&appid=b6907d289e10d714a6e88b30761fae22");
+            var temperatura = JsonConvert.DeserializeObject<Temperatura>(json);
+            var temperaturaC = Convert.ToDecimal(temperatura.main.temp) - 32 *(5/9);
+            temperatura.main.temp = temperaturaC.ToString();
+            return View(temperatura);
         }
 
-        public ActionResult About()
+        public ActionResult Contacto()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Creado por: Jose Carlos Gonzalez Ibarra";
 
             return View();
         }
 
         public ActionResult ObtenerPuntoInteres()
         {
-            using (DB_GEOModels geoModel = new DB_GEOModels())
+            using (ExamenPracticoBD contexto = new ExamenPracticoBD())
             {
-                var listPInteres = geoModel.PUNTOS_INTERES.Select(p => new {
-                    PInteresID = p.PInteresID,
-                    Latitud = p.Latitud,
-                    Longitud = p.Longitud,
-                    Descripcion = p.Descripcion,
-                    Venta = p.Venta,
-                    Zona = p.ZONA.Descripcion,
-                    ZonaID = p.ZonaID
+                var listaPuntoInteres = contexto.PuntosInteres.Select(a => new {
+                    PuntoInteres = a.PuntoInteres,
+                    Latitud = a.Latitud,
+                    Longitud = a.Longitud,
+                    Descripcion = a.Descripcion,
+                    Venta = a.Venta,
+                    Zona = a.Zona.Descripcion,
+                    IdZona = a.IdZona
                 }).ToList();
-                return Json(new { data = listPInteres }, JsonRequestBehavior.AllowGet);
+
+                if(listaPuntoInteres.Count > 0)
+                    return Json(new
+                    {
+                        Resultado = true,
+                        DatosPuntosInteres = listaPuntoInteres,
+                    }, JsonRequestBehavior.AllowGet);
+                else
+                    return Json(new
+                    {
+                        Resultado = false,
+                        Error = "No existen datos",
+                    }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult ObtenerSumaZonas()
+        {
+            var listaZonas = new List<string>();
+            var listaSumaZonas = new List<string>();
+
+            using (ExamenPracticoBD contexto = new ExamenPracticoBD())
+            {
+                var listaPuntoInteres = contexto.PuntosInteres.Select(a => new {
+                    PuntoInteres = a.PuntoInteres,
+                    Latitud = a.Latitud,
+                    Longitud = a.Longitud,
+                    Descripcion = a.Descripcion,
+                    Venta = a.Venta,
+                    Zona = a.Zona.Descripcion,
+                    IdZona = a.IdZona
+                }).ToList();
+
+                var ventas = listaPuntoInteres.GroupBy(x => x.Zona).Select(x => new
+                {
+                    zona = x.Key,
+                    total = x.Sum(y => y.Venta)
+                }).ToList();
+
+                var lSumaZonas = ventas.Select(v => new { nombre = v.zona, totalS = v.total });
+
+                foreach(var zona in lSumaZonas)
+                {
+                    listaZonas.Add(zona.nombre);
+                    listaSumaZonas.Add(zona.totalS.ToString());
+                }
+
+                if (ventas.Count > 0)
+                    return Json(new
+                    {
+                        Resultado = true,
+                        Zonas = listaZonas,
+                        SumaZonas = listaSumaZonas
+                    }, JsonRequestBehavior.AllowGet);
+                else
+                    return Json(new
+                    {
+                        Resultado = false,
+                        Error = "No existen datos",
+                    }, JsonRequestBehavior.AllowGet);
             }
         }
     }
---
+++ ExamenPractico/Models/Entidades/Temperatura.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamenPractico.Models.Entidades
+{
+    public class Temperatura
+    {
+        public Main main { get; set; }
+    }
+
+    public class Main
+    {
+        public string temp { get; set; }
+        public string pressure { get; set; }
+        public string humidity { get; set; }
+        public string temp_min { get; set; }
+        public string temp_max { get; set; }
+        public string sea_level { get; set; }
+        public string grnd_level { get; set; }
+    }
+}
